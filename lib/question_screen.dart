79ed93f74a6f417935fb42b1522093e19b290e80
Stefano Bianda <stefanobianda@ik.me>
2024-06-06 16:55:15 +0200

    preparation for the questions screen fix value at that time

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -18,16 +18,34 @@ class _QuestionScreenState extends State<QuestionScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return SizedBox(
+      width: double.infinity,
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'Question Screen',
+          const Text(
+            'Question Text',
             style: TextStyle(
               color: Colors.white,
               fontSize: 24,
             ),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4'),
           ),
         ],
       ),
