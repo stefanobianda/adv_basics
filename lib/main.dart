4d85be57d6bf45f617d9a1f5cc814359a17ad6a6
Stefano Bianda <stefanobianda@ik.me>
2024-06-06 14:57:23 +0200

    use Quiz widget on runApp method

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 93, 41, 183),
-                Color.fromARGB(255, 86, 73, 107),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
