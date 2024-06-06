d583d61a43d143531ca23abf06a254518d8d90d4
Stefano Bianda <stefanobianda@ik.me>
2024-06-06 11:21:38 +0200

    final step

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 93, 41, 183),
+                Color.fromARGB(255, 86, 73, 107),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
